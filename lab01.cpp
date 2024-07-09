3ebd452b7f30e3c364099ab28293fcb3644f284d
Nelson <mrnelsonlee@outlook.com>
2024-07-08 22:40:33 -0700

    EDITS

--- lab01.cpp
+++ lab01.cpp
@@ -1,5 +1,35 @@
+
+/*
+All code files to be submitted as part of the submission should have a name block at the top.
+The name block should clearly indicate
+the Lab Number,
+the name of the person, and
+a one line statement explaining the purpose of the assignment.
+Your code files should also include
+pre/post headers as discussed in the documentation guidelines in the first week of class, and
+have a comment block of pseudocode as necessary just before the relevant section of the code.
+Remember - pseudocode is not the same as inline comments or comments all over the code.
+Remember to include a 'pause' of some type before your program exits to allow me to observe the console output when I compile and run your program.
+ 
+
+Things to remember:
+
+Only submissions in ZIP file format will be accepted - no gzip, 7z, tar, rar or any other file format.
+Not including the name block will result in a 10% penalty.
+Not including screenshots will result in a 10% penalty.
+Not including pseudocode where necessary will result in a 20% penalty.
+Not including pre/post headers will result in a 20% penalty.
+*/
+
+/*
+LAB NUMBER: LAB 1
+NAME: NELSON LEE, CHRISTINA VO
+PURPOSE OF THIS ASSIGNMENT: 
+*/
+
+//PSEUDOCODE
+
+
 #include <iostream>
 
 using namespace std;
-
-//CODE GOES HERE
