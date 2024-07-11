31a58518fb4cd2b73a694712dd8a97e174a0ab37
Nelson <mrnelsonlee@outlook.com>
2024-07-10 21:42:05 -0700

    Formatting

--- lab01/lab01.cpp
+++ lab01/lab01.cpp
@@ -1,24 +1,8 @@
-
-/*
-Your code files should also include
-pre/post headers as discussed in the documentation guidelines in the first week of class, and
-have a comment block of pseudocode as necessary just before the relevant section of the code.
-Remember - pseudocode is not the same as inline comments or comments all over the code.
-Remember to include a 'pause' of some type before your program exits to allow me to observe the console output when I compile and run your program.
-Run your program in the console with the following inputs:
-NumInts = 5, Ints = 53 5099 1223 567 17
-NumInts = 4, Ints = 1871 8069 3581 6841
-Remember to take multiple screenshots of the both the runs so that they are clearly readable without needing to zoom in.
-For documentation, include your name block as well pre/post and pseudocode for the 3 functions which are not 'main'. For pre/post documentation and pseudocode examples,
-*/
-
 /*
 LAB NUMBER: LAB 1
 NAME: NELSON LEE, CHRISTINA VO
 PURPOSE OF THIS ASSIGNMENT:  DEMONSTRATING THE DIFFERENCE BETWEEN ITERATION AND RECURSION BY CHECKING IF ALL ELEMENTS OF AN ARRAY ARE PRIME.
 */
-
-
 
 #include <iostream>
 #include <cmath>
